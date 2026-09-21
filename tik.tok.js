9e9a90781715419218015d685844e95ee10be984
iki1uc <entertrainer@web.de>
2026-09-21 07:38:28 +0200

    Create tik.tok.js

---
+++ tik.tok.js
@@ -0,0 +1,4 @@
+HAVA.tick = 0   → nefes 12/min → hacim 500 mL → 86 mL O₂ bleibt → YÜZER
+HAVA.tick = 10  → nefes 15/min → hacim 625 mL → 107 mL O₂ → YÜZER
+HAVA.tick = 30  → nefes  8/min → hacim 333 mL →  57 mL O₂ → YÜZER (aber knapper)
+HAVA.tick = 60  → nefes 12/min → hacim 500 mL → 86 mL O₂ → YÜZER
